ffc1f314e30a8ad6713aa15527d434211dc868c1
An Nguyen <annguyen2007master@gmail.com>
2025-12-17 11:28:24 -0700

    Gui images

--- Gui.py
+++ Gui.py
@@ -1,31 +1,26 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         #initial the app
         super().__init__()
         self.setWindowTitle("MyApp")
+        self.setGeometry(300, 300, 500, 500)
 
-        #set up the outlook
-        self.setGeometry(0, 0, 300, 300)
-        self.setWindowIcon(QIcon("profile.jpg"))
+        label = QLabel(self)
+        label.setGeometry(0, 0, 250, 150)
 
-        #font
-        label = QLabel("Hello", self)
-        label.setFont(QFont("Arial", 50))
-        label.setGeometry(0, 0, 300, 80)
-        label.setStyleSheet("color: red;"
-                            "background-color: black;"
-                            "font-weight: bold;"
-                            "font-style: italic;"
-                            "text-decoration: underline;")
-        label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
+        pixmap = QPixmap("profile.jpg")
+        label.setPixmap(pixmap)
 
+        label.setScaledContents(True)
+        label.setGeometry((self.width() - label.width()) // 2,
+                          (self.height() - label.height()) //  2,
+                          label.width(),
+                          label.height())
 
 def main():
     #to run the app
