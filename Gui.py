f0fd5eccba3ced2fe6a91c95dabb50cb36e2a41d
An Nguyen <annguyen2007master@gmail.com>
2025-12-17 12:36:41 -0700

    Gui checkbox

--- Gui.py
+++ Gui.py
@@ -1,8 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication, QMainWindow,
-                             QPushButton, QLabel)
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QCheckBox)
 from PyQt5.QtCore import Qt
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,23 +10,21 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("MyApp")
         self.setGeometry(300, 300, 500, 500)
-        self.label = QLabel("Hello", self)
+        self.checkBox = QCheckBox("Do you like me?", self)
         self.initUI()
 
     def initUI(self):
-        self.button = QPushButton("Hit me", self)
-        self.button.setGeometry(150, 150, 200, 200)
-        self.button.setStyleSheet("font-size:30px;")
-        self.button.clicked.connect(self.on_click)
+        self.checkBox.setGeometry(0, 0, 300, 300)
+        self.checkBox.setStyleSheet("font-size:20px;"
+                                    "font-family:Verdana;")
+        self.checkBox.setCheckState(False)
+        self.checkBox.stateChanged.connect(self.checkbox_changed)
 
-        self.label.setGeometry(150, 50, 200, 200)
-        self.label.setAlignment(Qt.AlignHCenter)
-        self.label.setStyleSheet("font-size:30px;")
-
-    def on_click(self):
-        self.label.setText("You're GAY")
-
-
+    def checkbox_changed(self, state):
+        if Qt.CheckState(state):
+            print("you gay")
+        else:
+            print("get outta here")
 def main():
     #to run the app
     app = QApplication(sys.argv)
