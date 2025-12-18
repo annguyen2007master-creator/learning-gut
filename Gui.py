afeb3830af3d378d4a97a878c5babb93bd67a209
An Nguyen <annguyen2007master@gmail.com>
2025-12-18 11:34:49 -0700

    Gui radiobutton

--- Gui.py
+++ Gui.py
@@ -1,8 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QCheckBox)
-from PyQt5.QtCore import Qt
-
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QRadioButton, QButtonGroup)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,21 +8,38 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("MyApp")
         self.setGeometry(300, 300, 500, 500)
-        self.checkBox = QCheckBox("Do you like me?", self)
+        self.radio1 = QRadioButton("Pokemon", self)
+        self.radio2 = QRadioButton("Pikachu", self)
+        self.radio3 = QRadioButton("toi ngu", self)
+        self.buttonGroup1 = QButtonGroup(self)
+        self.buttonGroup2 = QButtonGroup(self)
         self.initUI()
 
     def initUI(self):
-        self.checkBox.setGeometry(0, 0, 300, 300)
-        self.checkBox.setStyleSheet("font-size:20px;"
-                                    "font-family:Verdana;")
-        self.checkBox.setCheckState(False)
-        self.checkBox.stateChanged.connect(self.checkbox_changed)
+        self.radio1.setGeometry(100, 100, 500, 50)
+        self.radio2.setGeometry(100, 150, 500, 50)
+        self.radio3.setGeometry(100, 200, 500, 50)
 
-    def checkbox_changed(self, state):
-        if Qt.CheckState(state):
-            print("you gay")
-        else:
-            print("get outta here")
+
+        self.setStyleSheet("QRadioButton{"
+                           "font-size:14px;"
+                           "font-family: arial;"
+                           "padding: 10px;"
+                           "}")
+
+        self.buttonGroup1.addButton(self.radio1)
+        self.buttonGroup1.addButton(self.radio2)
+        self.buttonGroup2.addButton(self.radio3)
+
+        self.radio1.toggled.connect(self.radio_button_clicked)
+        self.radio2.toggled.connect(self.radio_button_clicked)
+        self.radio3.toggled.connect(self.radio_button_clicked)
+
+    def radio_button_clicked(self):
+        radio_button = self.sender()
+        if radio_button.isChecked():
+            print(f"{radio_button.text()} is selected")
+
 def main():
     #to run the app
     app = QApplication(sys.argv)
